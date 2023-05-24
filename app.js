ce3554f30cebe97f9bc76d2e306e95f5cbf4c6bc
essienricch <essienrichard98@gmail.com>
2023-05-24 15:00:25 -0700

    re-save

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const env = require("dotenv");
+require("dotenv").config();
 const sequelize = require("./util/db");
 
-env.config();
+
 
 const app = express();
-const route = express.Router();
+// const route = express.Router();
 
 const user_router = require("./router/userRoutes");
 const post_router = require("./router/postRoutes");
@@ -20,19 +20,19 @@ app.use(express.json());
 app.use("/users", user_router);
 app.use("/post", post_router);
 
-// sequelize.sync( )
-// .then((result) =>
-//   {console.log(result)})
-//   .catch((error) => {console.log(error)});
+sequelize.sync( )
+.then((result) =>
+  {console.log(result)})
+  .catch((error) => {console.log(error)});
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((error) => {
-    console.log("Unable to connect to the database:", error);
-  });
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log("Connection has been established successfully.");
+//   })
+//   .catch((error) => {
+//     console.log("Unable to connect to the database:", error);
+//   });
 
 app.listen(process.env.PORT || 2000, () => console.log("Starting at 2000..."));
 
--- util/db.js
+++ util/db.js
@@ -1,10 +1,12 @@
 const Sequelize = require('sequelize');
+require('dotenv').config()
 
 
 const sequelize = new Sequelize(
+   
     process.env.PG_DB,
     process.env.PG_USER,
-    process.env.PG_PASSWORD,
+    process.env.PG_PASS,
     {
         host: process.env.PG_HOST,
         dialect: 'postgres',
