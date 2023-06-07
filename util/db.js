1363a5aeca033c11cb2b630274b03821ba750479
essienricch <essienrichard98@gmail.com>
2023-06-07 01:21:53 -0700

    updated branch

--- util/db.js
+++ util/db.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
+    process.env.DB_URL,
     {
         host: process.env.DB_HOST,
         dialect: 'postgres',
