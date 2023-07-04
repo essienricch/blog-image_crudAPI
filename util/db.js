98826c2bf3a4ee4eeb6a30805ffca5498ff92989
essienricch <essienrichard98@gmail.com>
2023-07-04 09:37:06 -0700

    update db

--- util/db.js
+++ util/db.js
@@ -2,15 +2,14 @@ const Sequelize = require('sequelize');
 require('dotenv').config()
 
 
-const sequelize = new Sequelize(
+const sequelize = new Sequelize({
    
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    process.env.DB_URL,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
     }
 );
 
